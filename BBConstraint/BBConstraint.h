1291cff34c7951c61c1a7dfbe483864a1ae2bdbf
Bobby <bobby.nguyen.goldenowl@gmail.com>
2023-05-08 12:31:11 +0700

    first commit

---
+++ BBConstraint/BBConstraint.h
@@ -0,0 +1,18 @@
+//
+//  BBConstraint.h
+//  BBConstraint
+//
+//  Created by innmac on 08/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBConstraint.
+FOUNDATION_EXPORT double BBConstraintVersionNumber;
+
+//! Project version string for BBConstraint.
+FOUNDATION_EXPORT const unsigned char BBConstraintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBConstraint/PublicHeader.h>
+
+
---
+++ BBConstraintExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BBConstraintExamples
+//
+//  Created by innmac on 08/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
